295ac9287ff72b42d484e1fdb54dc2a20ad39b8f
baaggii <baaggii@yahoo.com>
2025-05-02 17:34:15 +0800

    UI: товч ногоон болгов

--- src/server.js
+++ src/server.js
@@ -24,7 +24,7 @@ process.on('uncaughtException',  e => console.error('❌ Uncaught Exception:',  
 process.on('unhandledRejection', e => console.error('❌ Unhandled Rejection:', e));
 
 /* ---------- CORS ---------- */
-const PORT = process.env.API_PORT || 3001;
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, '0.0.0.0', () =>
   console.log(`✅ API ready on port ${PORT}`)
