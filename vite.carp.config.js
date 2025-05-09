506cd54779db93d20ae1794e50697b199af8bee3
baaggii <baaggii@yahoo.com>
2025-05-10 01:39:44 +0800

    UI: товч ногоон болгов

--- vite.carp.config.js
+++ vite.carp.config.js
@@ -10,27 +10,3 @@ module.exports = defineConfig({
     emptyOutDir: true
   }
 })
-
-
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { HashRouter, Routes, Route } from 'react-router-dom'
-import Home from './pages/Home'
-import Login from './pages/Login'
-import CarpenterPage from './pages/CarpenterPage'
-import ProductList from './pages/ProductList'
-import ProductDetail from './pages/ProductDetail'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <HashRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path=":username" element={<CarpenterPage />} />
-        <Route path=":username/products" element={<ProductList />} />
-        <Route path=":username/product:productId" element={<ProductDetail />} />
-      </Routes>
-    </HashRouter>
-  </React.StrictMode>
-)
