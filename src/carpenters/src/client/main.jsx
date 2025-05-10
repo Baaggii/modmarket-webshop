a56406f53f6370cfd2a76242c492fab9ff0b02c3
baaggii <baaggii@yahoo.com>
2025-05-11 01:43:03 +0800

    UI: товч ногоон болгов

--- src/carpenters/src/client/main.jsx
+++ src/carpenters/src/client/main.jsx
@@ -1,20 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import App from './pages/App'
-import Login from './pages/Login'
+import App from './App'
 import './index.css'
-
-console.log("Login loaded")
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="/login" element={<Login />} />
-    </Routes>
-  </BrowserRouter>
-
+    <App />
   </React.StrictMode>
 )
--- src/carpenters/src/client/pages/App.jsx
+++ src/carpenters/src/client/pages/App.jsx
@@ -1,7 +1,28 @@
+import React from 'react'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from 'react-router-dom'
+
+import Login from './pages/Login'
+import Register from './pages/Register'
+import Dashboard from './pages/Dashboard'
+import CarpenterPage from './pages/CarpenterPage'
+import ProductList from './pages/ProductList'
+import ProductDetail from './pages/ProductDetail'
+
 export default function App() {
   return (
-    <div className="p-6 text-xl font-bold text-green-700">
-      MM ERP Final Scaffold ✅
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path=":username" element={<CarpenterPage />} />
+        <Route path=":username/products" element={<ProductList />} />
+        <Route path=":username/product/:productId" element={<ProductDetail />} />
+      </Routes>
+    </Router>
   )
 }
