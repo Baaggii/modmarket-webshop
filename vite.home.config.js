e4208226bad4ad9f6533c7ac207ae87255df8310
baaggii <baaggii@yahoo.com>
2025-05-13 21:14:14 +0800

    UI: товч ногоон болгов

--- vite.home.config.js
+++ vite.home.config.js
@@ -5,6 +5,7 @@ module.exports = defineConfig({
   base: '/',
   plugins: [react()],
   root: 'src/homepage',
+  publicDir: '.', // Үндсэн директороос .htaccess зэргийг татах тохиргоо
   build: {
     outDir: '../../../../public_html',
     emptyOutDir: false
