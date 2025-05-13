8f81e65bee9cb865f51a11f01f9b4681e5da6ed8
baaggii <baaggii@yahoo.com>
2025-05-13 10:04:49 +0800

    UI: товч ногоон болгов

--- src/carpenters/src/client/pages/App.jsx
+++ src/carpenters/src/client/pages/App.jsx
@@ -5,6 +5,35 @@ import {
   Route
 } from 'react-router-dom'
 
+import Login from './Login'
+import Register from './Register'
+import Dashboard from './Dashboard'
+import CarpenterPage from './CarpenterPage'
+import ProductList from './ProductList'
+import ProductDetail from './ProductDetail'
+
+export default function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path=":username" element={<CarpenterPage />} />
+        <Route path=":username/products" element={<ProductList />} />
+        <Route path=":username/product/:productId" element={<ProductDetail />} />
+      </Routes>
+    </Router>
+  )
+}
+```jsx
+import React from 'react'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from 'react-router-dom'
+
 import Login from './pages/Login'
 import Register from './pages/Register'
 import Dashboard from './pages/Dashboard'
